31dcfa248ec2c2a7c981d516ea7b6bc7b4d3f8a4
ahnsam207 <2ok25@daum.net>
2025-09-11 10:24:27 +0900

    Upload 10403김동현_0911_3.py

---
+++ upload/class4/10403김동현_0911_3.py
@@ -0,0 +1,49 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+5+3
+8
+5-3
+2
+5*3
+15
+5/3
+1.6666666666666667
+5//3
+1
+5%3
+2
+5**3
+125
+"김경복"
+'김경복'
+"김경" + "복"
+'김경복'
+"김"+"경"+"복"
+'김경복'
+a = 3
+b = 3
+a + b
+6
+a=5
+a+b
+8
+성 + "김"
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    성 + "김"
+NameError: name '성' is not defined
+
+>>> 성 + "김"
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    성 + "김"
+NameError: name '성' is not defined
+>>> 성= "김"
+>>> 이름 + "경복""
+SyntaxError: unterminated string literal (detected at line 1)
+>>> 이름= "경복"
+>>> 성 + 이름
+'김경복'
+>>> 이름 = "경향"
+>>> 성+이름
+'김경향'
