4b9b414d29412715760c20a1e330e61434e52749
ahnsam207 <2ok25@daum.net>
2025-09-11 10:26:23 +0900

    Upload 10415이서준0911_3.py.py

---
+++ upload/class4/10415이서준0911_3.py.py
@@ -0,0 +1,110 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+k3
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    k3
+NameError: name 'k3' is not defined
+k3=7
+k3
+7
+k5=5
+>>> k5
+5
+>>> a=3
+>>> a
+3
+>>> type(k7)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    type(k7)
+NameError: name 'k7' is not defined. Did you mean: 'k3'?
+>>> type(k7)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    type(k7)
+NameError: name 'k7' is not defined. Did you mean: 'k3'?
+>>> type(k3)
+<class 'int'>
+>>> 10415이서준생년월일='2009년04월19일'
+SyntaxError: invalid syntax
+>>> 10415='이서준'
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+>>> '10415이서준생년월일'='2009년04월19일'
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+>>> "이"+"7"
+'이7'
+>>> *30
+SyntaxError: can't use starred expression here
+>>> 30*3
+90
+>>> 5//3
+1
+>>> 5/3
+1.6666666666666667
+>>> 7/3
+2.3333333333333335
+>>> 7/2
+3.5
+>>> 7/9
+0.7777777777777778
+>>> 5%3
+2
+5//3+5%3
+3
+3-5
+-2
+5=3
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+
+U.S="미국"
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    U.S="미국"
+NameError: name 'U' is not defined
+'이서준'+'10415'
+'이서준10415'
+'김'+'경'+'복'
+'김경복'
+ㅁ
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    ㅁ
+NameError: name 'ᄆ' is not defined
+a
+3
+이서준=10415
+이서준
+10415
+이름='이서준'
+이름'
+SyntaxError: unterminated string literal (detected at line 1)
+이름
+'이서준'
+이름=서준
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    이름=서준
+NameError: name '서준' is not defined. Did you mean: '이서준'?
+이름='서준'
+서준
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    서준
+NameError: name '서준' is not defined. Did you mean: '이서준'?
+서준='이름'
+이='성'
+이=서준
+이='성'
+이+서준
+'성이름'
+성='이'
+성+이름
+'이서준'
+학번='10415'
+성+이름+학번
+'이서준10415'
+학번+성+이름
+'10415이서준'
+학번+학번+이름+성
+'1041510415서준이'
