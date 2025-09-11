6a6a870b372c364f42bd1d80e4ea48e54288b604
ahnsam207 <2ok25@daum.net>
2025-09-11 10:24:36 +0900

    Upload 10417주민재_0911_3.py

---
+++ upload/class4/10417주민재_0911_3.py
@@ -0,0 +1,37 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 5+3
+8
+>>> 5-3
+2
+>>> 5*3
+15
+>>> 5/3
+1.6666666666666667
+>>> 5//3
+1
+>>> 5%3
+2
+>>> 5**3
+125
+>>> "김걍복"
+'김걍복'
+>>> "김"+"경복"
+'김경복'
+>>> "김경"+ "복
+SyntaxError: unterminated string literal (detected at line 1)
+>>> "김경"+"복"
+'김경복'
+>>> 
+>>> "김"+"경"+"복"
+'김경복'
+>>> a=5
+>>> b=3
+>>> a=b
+>>> a=5
+>>> a+b
+8
+>>> 성="김"
+>>> 이름="경복"
+>>> 성+이름
+'김경복'
