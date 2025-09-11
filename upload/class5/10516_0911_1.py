45981928173a706f35020b8131ff706d77b9c108
ahnsam207 <2ok25@daum.net>
2025-09-11 09:26:03 +0900

    Upload 10516_0911_1.py

---
+++ upload/class5/10516_0911_1.py
@@ -0,0 +1,206 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+import sys
+import sys
+
+
+
+let
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    let
+NameError: name 'let' is not defined. Did you mean: 'len'?
+async
+SyntaxError: invalid syntax
+
+
+
+
+home = true
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    home = true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+a = 3 + 1
+print(a)
+4
+type(7)
+<class 'int'>
+a2 = "공"
+print(' 공공공공공 + str {a}').format())
+SyntaxError: unmatched ')'
+SyntaxError: unmatched ')'
+SyntaxError: invalid syntax
+
+print(' 공공공공공 + str {a}').format()
+ 공공공공공 + str {a}
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    print(' 공공공공공 + str {a}').format()
+AttributeError: 'NoneType' object has no attribute 'format'
+print(' 공공공공공 + str {a}').format
+ 공공공공공 + str {a}
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print(' 공공공공공 + str {a}').format
+AttributeError: 'NoneType' object has no attribute 'format'
+AttributeError: 'NoneType' object has no attribute 'format'
+SyntaxError: invalid syntax
+
+
+
+
+print(' 공공공공공 + str {a}')
+ 공공공공공 + str {a}
+print(' 공공공공공 + a ').format())
+SyntaxError: unmatched ')'
+SyntaxError: unmatched ')'print(' 공공공공공 + a')
+SyntaxError: invalid syntax
+print(' 공공공공공 + a')print(' 공공공공공 + a')
+SyntaxError: invalid syntax
+
+
+
+print(' 공공공공공 + a')
+ 공공공공공 + a
+bool = false
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    bool = false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+
+ints = [1,2,3,4,5]
+int2 = [5,6,4,6,4,2]
+print(ints + int2)
+[1, 2, 3, 4, 5, 5, 6, 4, 6, 4, 2]
+home = true
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    home = true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+float(5)
+5.0
+float(5)
+5.0
+
+
+
+print
+<built-in function print>
+ints + int2
+[1, 2, 3, 4, 5, 5, 6, 4, 6, 4, 2]
+
+
+
+float(ints) + integer(int2)
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    float(ints) + integer(int2)
+TypeError: float() argument must be a string or a real number, not 'list'
+TypeError: float() argument must be a string or a real number, not 'list'
+SyntaxError: invalid syntax
+
+
+int(float("3+5)*3)
+          
+SyntaxError: unterminated string literal (detected at line 1)
+int(float('3+5')*3)
+...           
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    int(float('3+5')*3)
+ValueError: could not convert string to float: '3+5'
+>>> cls
+...           
+Traceback (most recent call last):
+  File "<pyshell#56>", line 1, in <module>
+    cls
+NameError: name 'cls' is not defined
+>>> class efe()
+SyntaxError: expected ':'
+>>> class ef e():
+...     
+SyntaxError: invalid syntax
+>>> class efe():
+...     def a1():
+...         return false
+...     end
+... 
+...     
+Traceback (most recent call last):
+  File "<pyshell#63>", line 1, in <module>
+    class efe():
+  File "<pyshell#63>", line 4, in efe
+    end
+NameError: name 'end' is not defined
+>>> NameError: name 'end' is not defined
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+    
+>>> 
+...             
+>>> class efe2():
+...     a = float(12)
+...     b = float(52)
+...     def abs(():
+...             
+SyntaxError: invalid syntax
+>>> 
+>>> class efe2():
+...     a = float(12)
+...     b = float(52)
+... 
+...     
+>>> 
+>>> 
+>>> 
+...         
+>>> class efe2():
+...     a = float(12)
+...     b = float(52)
+... 
+...         
+>>> 
+>>>     def abs"
+...     
+SyntaxError: unexpected indent
+>>> class efe2():
+...     a = float(12)
+...     b = float(52)
+...         def abs"
+...         
+SyntaxError: unexpected indent
+>>> 
+>>> 
+>>> 
+>>> 
+>>> class efe2():
+...     a = float(12)
+...     b = float(52)
+...     def abs:
+...         
+SyntaxError: expected '('
+>>> class efe2():
+...     a = float(12)
+...     b = float(52)
+... 
+...     
+>>> 
+>>> class efe2():
+...     a = float(12)
+...     b = float(52)
+...     def abs():
+...         return a + b
+... 
+...     
+>>> print(abs)
+<built-in function abs>
+>>> abs()
+Traceback (most recent call last):
+  File "<pyshell#99>", line 1, in <module>
+    abs()
+TypeError: abs() takes exactly one argument (0 given)
+>>> print( abs )
+<built-in function abs>
