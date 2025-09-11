b990b7f7f8eacb8713092bc545c20f0e2af5df55
ahnsam207 <2ok25@daum.net>
2025-09-11 10:11:21 +0900

    Upload 10409김태균-0911_2.py

---
+++ upload/class4/10409김태균-0911_2.py
@@ -0,0 +1,40 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> k3
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    k3
+NameError: name 'k3' is not defined
+>>> k3=7
+>>> k3
+7
+>>> k5="5"
+>>> k5
+'5'
+>>> type(k3)
+<class 'int'>
+>>> type(k5)
+<class 'str'>
+>>> 7=
+SyntaxError: cannot assign to literal
+>>> 
+>>> k7=7.5
+>>> k7
+7.5
+>>> type(k7)
+<class 'float'>
+>>> k3
+7
+>>> k5
+'5'
+>>> k7
+7.5
+>>> k3=k3+3
+>>> k3
+10
+>>> k3= k3+ int(k5)
+>>> k3
+15
+>>> k5= k5+ str(k7)
+>>> k5
+'57.5'
