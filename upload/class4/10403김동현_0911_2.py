8e849543c8052cf67a313b9f5670f920db721943
ahnsam207 <2ok25@daum.net>
2025-09-11 10:24:30 +0900

    Upload 10403김동현_0911_2.py

---
+++ upload/class4/10403김동현_0911_2.py
@@ -0,0 +1,47 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+k3=7
+k3
+7
+k5 = "5"
+k5
+'5'
+type(k3)
+<class 'int'>
+type(k5)
+<class 'str'>
+>>> k7 = 7.5
+>>> k7
+7.5
+>>> type(7.5)
+<class 'float'>
+>>> type(k7)
+<class 'float'>
+>>> k3 + k3 + 3
+17
+>>> k3
+7
+>>> k3+ k3 +3
+17
+>>> k3= k3+3
+>>> k3
+10
+>>> k3 += k3 + int(k5)
+>>> k3
+25
+>>> k3 = k3+int(k5)
+>>> k3
+30
+>>> ====k330
+SyntaxError: invalid syntax
+>>> 
+>>> k3= 10
+>>> k3
+10
+>>> k5=5
+>>> k5
+5
+>>> k3=k3+int(k5)
+>>> 
+>>> k3
+15
