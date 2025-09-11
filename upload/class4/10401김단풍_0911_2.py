d1a5c724c0f913e0791761a84f8500ea8aeceb31
ahnsam207 <2ok25@daum.net>
2025-09-11 10:09:30 +0900

    Upload 10401김단풍_0911_2.py

---
+++ upload/class4/10401김단풍_0911_2.py
@@ -0,0 +1,38 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> k3
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    k3
+NameError: name 'k3' is not defined
+>>> k3=7
+>>> k3+3
+10
+>>> k3
+7
+>>> k5 = "5"
+>>> k5
+'5'
+>>> type(k3)
+<class 'int'>
+>>> type(k5)
+<class 'str'>
+>>> k7 = 7.5
+>>> k7
+7.5
+>>> type(k7)
+<class 'float'>
+>>> k3+7
+14
+>>> k3
+7
+>>> k5
+'5'
+>>> k7
+7.5
+>>> k3 = k3 + 3
+>>> k3
+10
+>>> k3 = k3 + int(k5)
+>>> k3
+15
