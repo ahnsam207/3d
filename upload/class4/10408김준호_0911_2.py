e8cad03981f188f433cedd2b517ce03505d1d16b
ahnsam207 <2ok25@daum.net>
2025-09-11 10:12:16 +0900

    Upload 10408김준호_0911_2.py

---
+++ upload/class4/10408김준호_0911_2.py
@@ -0,0 +1,40 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> '7.5'
+'7.5'
+>>> k3 = 7
+>>> k3
+7
+>>> k5 = "5"
+>>> k5
+'5'
+>>> tupe(k3)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    tupe(k3)
+NameError: name 'tupe' is not defined. Did you mean: 'tuple'?
+>>> type(k3)
+<class 'int'>
+>>> type(k5)
+<class 'str'>
+>>> k7 = 7.5
+>>> type(k7)
+<class 'float'>
+>>> k3 = k3 +7
+>>> k3 = k3 + 7
+>>> k3 = k3 + int(k5)
+>>> k3 = 7
+>>> k3
+7
+>>> k3 = k3 + 3
+>>> k3
+10
+>>> k3 = k3 = int(k5)
+>>> k3
+5
+>>> k3 = k3 + int(k5)
+>>> k3
+10
+>>> k3 = k3 + int(k5)
+>>> k3
+15
