50ed11877e150b7e85a31c4a33f3ecd155518523
ahnsam207 <2ok25@daum.net>
2025-09-12 09:11:28 +0900

    Upload 10514임소리_0912_2.py

---
+++ upload/class5/10514임소리_0912_2.py
@@ -0,0 +1,32 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> k3 = 7
+>>> type(k3)
+<class 'int'>
+>>> k5 = "5"
+>>> type(k5)
+<class 'str'>
+>>> k7 = 7.5
+>>> type-k7
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    type-k7
+TypeError: unsupported operand type(s) for -: 'type' and 'float'
+>>> type(k7)
+<class 'float'>
+>>> k3=k3+3
+>>> k3
+10
+>>> k3=k3+int('7')
+>>> k3
+17
+>>> k3=k3+k5
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    k3=k3+k5
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> k3=k3+int(k5)
+>>> k3
+22
+>>> type(k3)
+<class 'int'>
