a7b024f06e732108b1daef5fa2f1c4e656db6387
ahnsam207 <2ok25@daum.net>
2025-09-12 09:25:26 +0900

    Upload 10508배재윤_0912_3.py

---
+++ upload/class5/10508배재윤_0912_3.py
@@ -0,0 +1,44 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+5=3
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+5+3
+8
+>>> 5-3
+2
+>>> 5*3
+15
+>>> 5/3
+1.6666666666666667
+>>> 5//3
+1
+>>> 5%2
+1
+>>> 5**3
+125
+>>> "배재"+"윤"
+'배재윤'
+>>> "배"+"재"+"윤"
+'배재윤'
+>>> "*"*10
+'**********'
+>>> "배재윤"*10
+'배재윤배재윤배재윤배재윤배재윤배재윤배재윤배재윤배재윤배재윤'
+>>> a=10
+>>> b=7
+>>> a+b
+17
+>>> a*b
+70
+>>> 성="김"
+>>> 이름="경복"
+>>> name=성+이름
+>>> name
+'김경복'
+>>> nmae*3
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    nmae*3
+NameError: name 'nmae' is not defined
+>>> name*3
+'김경복김경복김경복'
