d0624c3acd0f9da38698e71fa1e4a9ce8f759ec6
ahnsam207 <2ok25@daum.net>
2025-09-12 09:30:12 +0900

    Upload 10512윤시원_0912_3py.py

---
+++ upload/class5/10512윤시원_0912_3py.py
@@ -0,0 +1,62 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+5+3
+8
+5-3
+2
+5*3
+15
+5/3
+1.6666666666666667
+5//3
+1
+5%3
+2
+>>> 5**3
+125
+>>> 
+>>>  "김경"+"복"
+...  
+SyntaxError: unexpected indent
+>>> "김"+"경복"
+'김경복'
+>>> "김"+"경"+"복"
+'김경복'
+>>> a10
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    a10
+NameError: name 'a10' is not defined
+>>> b=7
+>>> a=10
+>>> a+b
+17
+>>> a-b
+3
+>>> a*n
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    a*n
+NameError: name 'n' is not defined
+>>> 성
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    성
+NameError: name '성' is not defined
+>>> 이름=
+SyntaxError: invalid syntax
+>>> name
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    name
+NameError: name 'name' is not defined
+>>> name= 성+이름
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    name= 성+이름
+NameError: name '성' is not defined
+>>> name
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    name
+NameError: name 'name' is not defined
