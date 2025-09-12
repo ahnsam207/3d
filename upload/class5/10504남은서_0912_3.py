6d222efa803f65cebdfb8ec35f2a9f36a5ad2efb
ahnsam207 <2ok25@daum.net>
2025-09-12 09:26:41 +0900

    Upload 10504남은서_0912_3.py

---
+++ upload/class5/10504남은서_0912_3.py
@@ -0,0 +1,49 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+5+3
+8
+5-3
+2
+5*3
+15
+5/3
+1.6666666666666667
+>>> 5//3
+1
+>>> 5%3
+2
+>>> 5**3
+125
+>>> "김경"+"복"
+'김경복'
+>>> "김"+"경복"
+'김경복'
+>>> "김"+"경"+"복"
+'김경복'
+>>> "김경복"
+'김경복'
+>>> "*"*10
+'**********'
+>>> a=10
+>>> b+7
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    b+7
+NameError: name 'b' is not defined
+>>> a+b
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    a+b
+NameError: name 'b' is not defined
+>>> b=7
+>>> a+b
+17
+>>> a-b
+3
+>>> a*b
+70
+>>> 성="김"
+>>> 이름="경복"
+>>> name=성+이름
+>>> name
+'김경복'
