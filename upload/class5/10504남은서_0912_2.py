777520d99e9dcdec3987f0c33198b06e74999500
ahnsam207 <2ok25@daum.net>
2025-09-12 09:26:37 +0900

    Upload 10504남은서_0912_2.py

---
+++ upload/class5/10504남은서_0912_2.py
@@ -0,0 +1,57 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+k3
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    k3
+NameError: name 'k3' is not defined
+k3=7
+k3
+7
+type(k3)
+<class 'int'>
+k5="5"
+k5
+'5'
+typr(k5)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    typr(k5)
+NameError: name 'typr' is not defined. Did you mean: 'type'?
+>>> type(k5)
+<class 'str'>
+>>> k7=7.5
+>>> k7
+7.5
+>>> typr(k7)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    typr(k7)
+NameError: name 'typr' is not defined. Did you mean: 'type'?
+>>> type(k7)
+<class 'float'>
+>>> k3
+7
+>>> k5
+'5'
+>>> k7
+7.5
+>>> k3=k3+3
+>>> k3
+10
+>>> k3=k3+"7"
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    k3=k3+"7"
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> k3=k3+int("7")
+>>> k3
+17
+>>> k3=k3+k5
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    k3=k3+k5
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> k3=k3+int(k5)
+>>> k3
+22
