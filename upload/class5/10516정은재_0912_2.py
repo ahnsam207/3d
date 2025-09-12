c44e93fbfebad7c3d1ec734f5f4acf3a938e9b11
ahnsam207 <2ok25@daum.net>
2025-09-12 09:25:45 +0900

    Upload 10516정은재_0912_2.py

---
+++ upload/class5/10516정은재_0912_2.py
@@ -0,0 +1,39 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a2 = 1
+>>> 5a = a2 * 2
+SyntaxError: invalid decimal literal
+>>> 5a = a2 + 2
+SyntaxError: invalid decimal literal
+>>> 1a = a2 + 2
+SyntaxError: invalid decimal literal
+>>> a1 = a2 + 2
+>>> print(a1)
+3
+>>> 3
+3
+>>> d1 = 3
+>>> print(d1)
+3
+>>> d5 = str
+>>> d2 = int
+>>> print(d2(1) + 2))
+SyntaxError: unmatched ')'
+>>> print(d2(1) + 1)
+2
+>>> printt
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    printt
+NameError: name 'printt' is not defined. Did you mean: 'print'?
+>>> 
+>>> print(d2(10) + a2)
+11
+>>> 5a = a2 + 2
+SyntaxError: invalid decimal literal
+>>> a2
+1
+>>> d2
+<class 'int'>
+>>> d2
+<class 'int'>
