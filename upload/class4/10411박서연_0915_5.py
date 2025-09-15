14cbe036c2f1f3d917bb78bd19c29a6f05fc74a5
ahnsam207 <2ok25@daum.net>
2025-09-15 12:22:50 +0900

    Upload 10411박서연_0915_5.py

---
+++ upload/class4/10411박서연_0915_5.py
@@ -0,0 +1,19 @@
+a=8
+b=3
+
+pr int(str(a)+"+"+str(b)+"="+str(a+b))
+pr int(a-b)
+pr int(a*b)
+pr int(a/b)
+pr int(a//b)
+pr int(a%b)
+pr int(a**b)
+
+성="안"
+이름="경향"
+
+pr int("김경복")
+pr int("김경"+"복")
+pr int(성+이름)
+pr int(성+"경"+"복")
+pr int("당신의 이름은 성은 "+성+"이고 이름은 "+이름+"입니다.")
