0d4848f8f157462f1b3db933b074741b36439b62
ahnsam207 <2ok25@daum.net>
2025-09-15 09:28:31 +0900

    Upload 10505박광길_09015_5.py

---
+++ upload/class5/10505박광길_09015_5.py
@@ -0,0 +1,18 @@
+a=8
+b=5
+
+print(str(a)+"+"+str(b)+"="+str(a+b))
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+성="박"
+이름="광길"
+print("김경복")
+print("김"+"경"+"복")
+print("김경"+"복")
+print("김"+"경복")
+print("당신의 이름 성은 "+성+" 이름은 "+이름+"입니다.")
