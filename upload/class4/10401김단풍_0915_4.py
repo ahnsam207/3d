2d4a15d5cb02bd974f4bee957a1952cba18c6c95
ahnsam207 <2ok25@daum.net>
2025-09-15 12:24:19 +0900

    Upload 10401김단풍_0915_4.py

---
+++ upload/class4/10401김단풍_0915_4.py
@@ -0,0 +1,15 @@
+a = 7
+b = 4
+
+
+print(a + b)
+print(a - b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print("김경복")
+print("김경" + "복") 
+print("김" + "경복")
+print("김" + "경" + "복")
