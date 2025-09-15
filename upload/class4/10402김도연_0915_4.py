803f1742f9d90ae596b1f9dd0d343b4d0548f6eb
ahnsam207 <2ok25@daum.net>
2025-09-15 12:24:27 +0900

    Upload 10402김도연_0915_4.py

---
+++ upload/class4/10402김도연_0915_4.py
@@ -0,0 +1,19 @@
+a = 7
+b = 4
+
+print(str(a)+"+"+ str(b)+"="+str(a+b))
+print(str(a)+"-"+ str(b)+"="+str(a-b))
+print(str(a)+"*"+ str(b)+"="+str(a*b))
+print(str(a)+"/"+ str(b)+"="+str(a/b))
+print(str(a)+"//"+ str(b)+"="+str(a//b))
+print(str(a)+"%"+ str(b)+"="+str(a%b))
+print(str(a)+"**"+ str(b)+"="+str(a**b))
+
+last_name = "안"
+name = "경향"
+
+print("김경복")
+print("김경"+"복")
+print(last_name+name)
+print("김"+"경"+"복")
+print("당신의 이름의 성은",last_name,"이고 이름은",name,"입니다.")
