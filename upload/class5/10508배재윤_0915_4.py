6f0ed0e1d0980227aa72b3ef9d4dc1169e1a5a96
ahnsam207 <2ok25@daum.net>
2025-09-15 09:23:40 +0900

    Upload 10508배재윤_0915_4.py

---
+++ upload/class5/10508배재윤_0915_4.py
@@ -0,0 +1,91 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+5-3
+2
+5/3
+1.6666666666666667
+5%3
+2
+5*1
+5
+5@1
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    5@1
+TypeError: unsupported operand type(s) for @: 'int' and 'int'
+5^1
+4
+"김경복"**3
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    "김경복"**3
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+김경복**3
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    김경복**3
+NameError: name '김경복' is not defined
+
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0915_4.py ==============
+
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0915_4.py ==============
+2
+15
+8
+2
+김경복
+김경복
+김경복
+김경복
+
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0915_4.py ==============
+4
+77
+18
+4
+김경복
+김경복
+김경복
+김경복
+
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0915_4.py ==============
+3
+40
+13
+3
+김경복
+김경복
+김경복
+김경복
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0915_4.py ==============
+8+5=13
+40
+13
+3
+김경복
+김경복
+김경복
+김경복
+>>> 
+=============== RESTART: C:\Users\user\Desktop\10508배재윤_0915_5.py ==============
+8+5=13
+40
+13
+3
+김경복
+김경복
+김경복
+김경복
+당신의 이름은, 성은김이름은경복입니다.
+>>> 
+=============== RESTART: C:\Users\user\Desktop\10508배재윤_0915_5.py ==============
+8+5=13
+40
+13
+3
+김경복
+김경복
+김경복
+김경복
+당신의 이름은, 성은안이름은경향입니다.
