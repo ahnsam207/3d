cfc80584f8cff89d8f7b2ddd8a998b8f8706836f
ahnsam207 <2ok25@daum.net>
2025-09-15 09:24:30 +0900

    Upload 10514임소리_0915_4.py

---
+++ upload/class5/10514임소리_0915_4.py
@@ -0,0 +1,16 @@
+a = 8
+b = 5
+
+print(str(a) +'+' + str(b) + '=' + str(a+b))
+print(str(a) + '-' + str(b) + '=' + str(a-b))
+print(str(a) + '*' + str(b) + '=' + str(a*b))
+print(str(a) + '/' + str(b) + '=' + str(a/b))
+print(str(a) + '//' + str(b) + '=' + str(a//b))
+print(str(a) + '%' + str(b) + '=' + str(a%b))
+print(str(a) + '**' + str(b) + '=' + str(a**b))
+'
+
+print('김경복')
+print('김' + '경복')
+print('김경' + '복')
+print('김'  + '경' + '복')
