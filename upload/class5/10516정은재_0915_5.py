e14a3e52bbc1ba269e2d6d4739be8fb6a7562cef
ahnsam207 <2ok25@daum.net>
2025-09-15 09:27:18 +0900

    Upload 10516정은재_0915_5.py

---
+++ upload/class5/10516정은재_0915_5.py
@@ -0,0 +1,54 @@
+import sys
+import random 
+
+def log(msg:str, num:int,op:bool):
+    print(msg,num,op)
+
+
+async def Ifloat():
+    a = 52
+
+
+
+async def Fint():
+    b = 64
+
+
+def world2():
+    a0 = False
+    a1= True
+
+
+
+    
+print(5 + 3)
+a = 4
+b = 6
+c = float
+print(a + b / c(52))
+ab = a + b / c(52)
+a2 = random
+log("랜덤 확률",ab,True)
+print(a2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
