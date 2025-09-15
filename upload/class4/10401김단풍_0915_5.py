8adb77a9e24b6aa840a3261c2ff9218dfb625b86
ahnsam207 <2ok25@daum.net>
2025-09-15 12:24:23 +0900

    Upload 10401김단풍_0915_5.py

---
+++ upload/class4/10401김단풍_0915_5.py
@@ -0,0 +1,21 @@
+a = 7
+b = 4
+
+
+print(str(a) + "+" + str(b) + "=" + (a + b))
+print(str(a) + "-" + str(b) + "=" + (a - b))
+print(str(a) + "*" + str(b) + "=" + (a * b))
+print(str(a) + "/" + str(b) + "=" + (a / b))
+print(str(a) + "//" + str(b) + "=" + (a // b))
+print(str(a) + "%" + str(b) + "=" + (a % b))
+print(str(a) + "**" + str(b) + "=" + (a ** b))
+
+성 = "김"
+이름 = "경복"
+
+print("김경복")
+print("김경" + "복") 
+print("김" + "경복")
+print(성 + 이름)
+print(성 + "경" + "복")
+print("당신의 이름은 성은" + 성 + "이고 이름은" + 이름 + "입니다.")
