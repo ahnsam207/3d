bf08de5ea0c5a67e49b27aca34394941c344f9e9
ahnsam207 <2ok25@daum.net>
2025-09-15 09:26:30 +0900

    Upload 10518 최영은_0915_4.py

---
+++ upload/class5/10518 최영은_0915_4.py
@@ -0,0 +1,18 @@
+a=8
+b=5
+print(str(a)+"+"+str(b)+"+"+str(a+b))
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+성="안"
+이름= "경향"
+
+print("김경복")
+print(성 +이름)
+print("김경"+"복")
+print(성+"경"+"복")
+print("당시의 이름은, 성은 " + 성 + ", 이름은 " + 이름 +"입니다.")
