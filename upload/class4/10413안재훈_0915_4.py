73a16502c5a12355be3284906b670ec13476223a
ahnsam207 <2ok25@daum.net>
2025-09-15 12:23:09 +0900

    Upload 10413안재훈_0915_4.py

---
+++ upload/class4/10413안재훈_0915_4.py
@@ -0,0 +1,17 @@
+a = 8
+b = 3
+
+
+
+print(a - b)
+print(a + b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print("김경복")
+print("김경" + "복")
+print("김" + "경복")
+print("김" + "경" + "복")
