d6abb78c56de0de485e53376c890e39657a16594
ahnsam207 <2ok25@daum.net>
2025-09-15 12:31:52 +0900

    Upload 10404김려함-0915-5..py

---
+++ upload/class4/10404김려함-0915-5..py
@@ -0,0 +1,19 @@
+a= 7
+b=4
+
+print(str(a)+"+"+str(b)+"="+str(a+b))
+print(str(a)+"-"+str(b)+"="+str(a+b))
+print(str(a)+"*"+str(b)+"="+str(a+b))
+print(str(a)+"/"+str(b)+"="+str(a+b))
+print(str(a)+"//"+str(b)+"="+str(a+b))
+print(str(a)+"%"+str(b)+"="+str(a+b))
+print(str(a)+"**"+str(b)+"="+str(a+b))
+
+성="김"
+이름="경복"
+
+print("김경복")
+print("김경"+"복")
+print("김"+"경복")
+print("김"+"경"+"복")
+print("당신의 이름은 성은"+성+"이고 이름은 "+이름+"입니다.")
