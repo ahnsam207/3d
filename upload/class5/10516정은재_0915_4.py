f3b5df5d6a2610a717bc263908169391be49ec0d
ahnsam207 <2ok25@daum.net>
2025-09-15 09:27:13 +0900

    Upload 10516정은재_0915_4.py

---
+++ upload/class5/10516정은재_0915_4.py
@@ -0,0 +1,42 @@
+import sys
+
+
+def log(msg:str, num:int,op:bool):
+    print(msg,num,op)
+
+async def Ifloat():
+    a = 52
+
+
+
+async def Fint():
+    b = 64
+
+
+    
+print(5 + 3)
+a = 4
+b = 6
+c = float
+print(a + b / c(52))
+ab = a + b / c(52)
+log("asdf",ab,True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
