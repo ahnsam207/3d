36f46eb2edd974ac34957ffdee2bc48ea96a79dd
ahnsam207 <2ok25@daum.net>
2025-09-15 12:23:06 +0900

    Upload 10413안재훈_0915_5.py

---
+++ upload/class4/10413안재훈_0915_5.py
@@ -0,0 +1,24 @@
+a = 7
+b = 3
+
+
+
+print(str(a) + "-" + str(b) + "=" + str(a+b))
+print(str(a) + "+" + str(b) + "=" + str(a+b))
+print(str(a) + "*" + str(b) + "=" + str(a+b))
+print(str(a) + "/" + str(b) + "=" + str(a+b))
+print(str(a) + "//" + str(b) + "=" + str(a+b))
+print(str(a) + "%" + str(b) + "=" + str(a+b))
+print(str(a) + "**" + str(b) + "=" + str(a+b))
+
+
+성 = "안"
+이름 = "재훈"
+
+
+
+print("김경복")
+print("김경" + "복")
+print(성 + 이름)
+print(성 + "경" + "복")
+print("당신의 성은 " + 성 + "이고 이름은 " + 이름 + "입니다" )
