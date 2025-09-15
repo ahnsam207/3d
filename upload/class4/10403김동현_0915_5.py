8991f089c8f92dc6834b89e0d6bd611e964af944
ahnsam207 <2ok25@daum.net>
2025-09-15 12:23:07 +0900

    Upload 10403김동현_0915_5.py

---
+++ upload/class4/10403김동현_0915_5.py
@@ -0,0 +1,18 @@
+a = 8
+b = 3
+print (str(a) + "+" + str(b) + "=" + str(a + b))
+print(str(a) + "-" + str(b) + "=" + str(a- b))
+print(str(a) + "*" + str(b) + " =" + str(a*b))
+print(str(a) + "/" + str(b) + " =" + str(a / b))
+print(str(a) + "//" + str(b) + " =" + str(a // b))
+print(str(a) + "%" + str(b) + " =" + str(a % b))
+print(str(a) + "**" + str(b) + " =" + str(a ** b))
+
+성 = "안"
+이름 = "경향"
+
+print("김경복")
+print("김경"+"복")
+print("김"+ "경복")
+print("김" + "경"+ "복")
+print("당신의 이름은 성은 " + 성 + "이고 이름은" + 이름 + "입니다.")
