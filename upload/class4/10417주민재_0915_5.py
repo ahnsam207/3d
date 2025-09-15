e7fab7db07888f147c57f9baf6cc84fea5309615
ahnsam207 <2ok25@daum.net>
2025-09-15 12:24:40 +0900

    Upload 10417주민재_0915_5.py

---
+++ upload/class4/10417주민재_0915_5.py
@@ -0,0 +1,19 @@
+a=53
+b=6
+
+print(str(a)+"+"+str(b)+"="+str(a+b))
+print(str(a)+"-"+str(b)+"="+str(a-b))
+print(str(a)+"*"+str(b)+"="+str(a*b))
+print(str(a)+"/"+str(b)+"="+str(a/b))
+print(str(a)+"//"+str(b)+"="+str(a//b))
+print(str(a)+"**"+str(b)+"="+str(a**b))
+
+성="김"
+이름="경복"      
+
+
+print("김경복")
+print("김경"+"복")
+print(성+이름)
+print(성+"경"+"복")
+print("당신의 이름은 서은"+성+"이고 이름은"+이름+"입니다")
