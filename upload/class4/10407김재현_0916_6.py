a3caaa3782884c13ac83e7165b314f5947215644
ahnsam207 <2ok25@daum.net>
2025-09-16 16:11:50 +0900

    Upload 10407김재현_0916_6.py

---
+++ upload/class4/10407김재현_0916_6.py
@@ -0,0 +1,37 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> input()
+5
+'5'
+>>> int(input())
+5
+5
+>>> days=input()
+5
+>>> days
+'5'
+>>> days=input()
+days= input ("일수를 입력하세요")
+>>> 일수를 입력하세요
+SyntaxError: invalid syntax
+>>> 
+>>> days=input("일수를 입력하세요")
+일수를 입력하세요5
+>>> days5
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    days5
+NameError: name 'days5' is not defined. Did you mean: 'days'?
+>>> input()
+input('나이를 입력하세요:')
+"input('나이를 입력하세요:')"
+>>> ':19'
+':19'
+>>> days=input('일수를 입력하세요.')
+일수를 입력하세요.5
+>>> days
+'5'
+>>> days=int(input('일수를 입력하세요:'))
+일수를 입력하세요:5
+>>> days
+5
