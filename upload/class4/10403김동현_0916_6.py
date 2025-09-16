c42b2aca02c78ea81e2606d7e018d42f68c2e752
ahnsam207 <2ok25@daum.net>
2025-09-16 16:10:30 +0900

    Upload 10403김동현_0916_6.py

---
+++ upload/class4/10403김동현_0916_6.py
@@ -0,0 +1,16 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> days = int ( input ())
+5
+>>> days= input()
+
+>>> days = input( )
+5
+>>> days = input( '일수를 입력하세요 : ' )
+일수를 입력하세요 : 5
+>>> days
+'5'
+>>> days = int ( input ( '일수를 입력하세요 : ' ))
+일수를 입력하세요 : 5
+>>> days
+5
