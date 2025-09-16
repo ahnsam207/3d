2ce136fe7c20caa2976ef1c4bab29f758fd3724a
ahnsam207 <2ok25@daum.net>
2025-09-16 14:05:46 +0900

    Upload 10501권정현_0916_6.py

---
+++ upload/class5/10501권정현_0916_6.py
@@ -0,0 +1,73 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+input
+<built-in function input>
+input()
+5
+'5'
+days = input()
+5
+days
+'5'
+5+days
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    5+days
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+5+int(days)
+10
+input("나이를 입력하세요:')
+      
+SyntaxError: unterminated string literal (detected at line 1)
+input('나이를 입력하세요:')
+      
+나이를 입력하세요:19
+'19'
+days=input('일수를 입력하세요.')
+      
+일수를 입력하세요.5
+days
+      
+'5'
+days=int(input('일수를 입력하세요:'))
+      
+일수를 입력하세요:5
+days
+      
+5
+
+=============== RESTART: C:/Users/user/Desktop/10501권정현_0916_7.py ==============
+8+5=13
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10501권정현_0916_7.py ==============
+첫번째 숫자 입력:8
+두번째 숫자 입력:5
+8+5=85
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10501권정현_0916_7.py ==============
+첫번째 숫자 입력:7
+두번째 숫자 입력:3
+7+3=10
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10501권정현_0916_7.py ==============
+첫번째 숫자 입력:7
+두번째 숫자 입력:3
+7-3=4
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10501권정현_0916_7.py ==============
+첫번째 숫자 입력:7
+두번째 숫자 입력:3
+7*3=21
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10501권정현_0916_7.py ==============
+첫번째 숫자 입력:7
+두번째 숫자 입력:3
+7/3=2.3333333333333335
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10501권정현_0916_7.py ==============
+첫번째 숫자 입력:7
+두번째 숫자 입력:3
+7+3=10
+7/3=2.3333333333333335
+7-3=4
+7*3=21
