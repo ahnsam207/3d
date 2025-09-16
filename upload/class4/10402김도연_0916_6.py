4e758c87dec07a3e03357ebbce41ca86e7048604
ahnsam207 <2ok25@daum.net>
2025-09-16 16:10:52 +0900

    Upload 10402김도연_0916_6.py

---
+++ upload/class4/10402김도연_0916_6.py
@@ -0,0 +1,45 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/user/Desktop/10402김도연_0916_5.py
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10402김도연_0916_5.py", line 1, in <module>
+    print = red
+NameError: name 'red' is not defined
+
+= RESTART: C:/Users/user/Desktop/10402김도연_0916_5.py
+5
+
+
+
+
+days = int(input())
+5
+days = int(input())
+print(days)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    days = int(input())
+ValueError: invalid literal for int() with base 10: 'print(days)'
+>>> 
+= RESTART: C:/Users/user/Desktop/10402김도연_0916_5.py
+5
+5
+>>> print(days)
+5
+>>> days = int(input('일수를 입력하세요:')
+...            5
+...            
+SyntaxError: '(' was never closed
+>>> days = int(input('일수를 입력하세요:'))
+...            
+일수를 입력하세요:5
+>>> day
+...            
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    day
+NameError: name 'day' is not defined. Did you mean: 'days'?
+>>> days
+...            
+5
