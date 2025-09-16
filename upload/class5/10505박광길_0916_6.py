c16bbd538572c6669bbbd0b35b2d72ed92aee4ec
ahnsam207 <2ok25@daum.net>
2025-09-16 14:08:11 +0900

    Upload 10505박광길_0916_6.py

---
+++ upload/class5/10505박광길_0916_6.py
@@ -0,0 +1,64 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+input()
+5
+'5'
+days
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    days
+NameError: name 'days' is not defined
+days=input()
+5
+days
+'5'
+5+(days)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    5+(days)
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+5+int(days)
+10
+input()
+
+''
+input('나이를 입력하세요요:')
+나이를 입력하세요요:
+''
+나이를 입력하세요요:
+    
+SyntaxError: invalid syntax
+
+
+
+
+input('나이를 입력하세요:')
+나이를 입력하세요:19
+'19'
+days=input('일수를 입력하세요.')
+일수를 입력하세요.5
+days
+'5'
+days=int(input('일수를 입력하세요:'))
+일수를 입력하세요:5
+days
+5
+>>> 
+==================== RESTART: C:/Users/user/Desktop/10505박광길_0916_7.py ====================
+8+5=13
+>>> 
+==================== RESTART: C:/Users/user/Desktop/10505박광길_0916_7.py ====================
+첫번째 숫자 입력:8
+두번째 숫자 입력:5
+8+5=85
+>>> 
+>>> 
+==================== RESTART: C:/Users/user/Desktop/10505박광길_0916_7.py ====================
+첫번째 숫자 입력:7
+두번째 숫자 입력:3
+7+3=73
+>>> 
+==================== RESTART: C:/Users/user/Desktop/10505박광길_0916_7.py ====================
+첫번째 숫자 입력:7
+두번째 숫자 입력:3
+7+3=10
