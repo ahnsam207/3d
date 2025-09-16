bffa85c7f5ca54746bdac83f42757ef23efac41a
ahnsam207 <2ok25@daum.net>
2025-09-16 16:11:50 +0900

    Upload 10415이서준_0916_6.py

---
+++ upload/class4/10415이서준_0916_6.py
@@ -0,0 +1,26 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> input()
+5
+'5'
+>>> int(input())
+5
+5
+>>> q
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    q
+NameError: name 'q' is not defined
+>>> days = int(input())
+5
+>>> 
+>>> days = input('일수를 입력하세요:')
+일수를 입력하세요:5
+>>> 5
+5
+>>> days
+'5'
+>>> days = int(input('일수를 입력하세요:'))
+일수를 입력하세요:5
+>>> days
+5
