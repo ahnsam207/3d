fe2f368ca831291bec652bd73f213093ef8c4748
ahnsam207 <2ok25@daum.net>
2025-09-16 16:12:30 +0900

    Upload 10405김세은_0916_6.py

---
+++ upload/class4/10405김세은_0916_6.py
@@ -0,0 +1,38 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> input()
+5
+'5'
+>>> int(input())
+5
+5
+>>> days=input()
+5
+>>> days
+'5'
+>>> days=input()
+5
+>>> days=input('일수를 입력하세요:')
+일수를 입력하세요:
+>>> 일수를 입력하세요:5
+SyntaxError: invalid syntax
+>>> days=int(input('일수를 입력하세요:'))
+일수를 입력하세요:5
+>>> days
+5
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10405김세은_0916_7.py ==============
+8+7=15
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10405김세은_0916_7.py ==============
+첫번째 숫자 입력: 11
+두번째 숫자 입력: 7
+ 11+ 7= 11 7
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10405김세은_0916_7.py ==============
+첫번째 숫자 입력:11
+두번째 숫자 입력:7
+11+7=18
+11-7=4
+11*7=77
+11/7=1.5714285714285714
