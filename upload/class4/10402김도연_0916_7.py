64054b5eb1f7b517cbec3358fe7a39487798169b
ahnsam207 <2ok25@daum.net>
2025-09-16 16:10:39 +0900

    Upload 10402김도연_0916_7.py

---
+++ upload/class4/10402김도연_0916_7.py
@@ -0,0 +1,7 @@
+a = int(input("첫 번째 숫자 입력:"))
+b = int(input("두 번째 숫자 입력:"))
+
+print(str(a)+"+"+str(b)+"="+str(a+b))
+print(str(a)+"-"+str(b)+"="+str(a-b))
+print(str(a)+"*"+str(b)+"="+str(a*b))
+print(str(a)+"/"+str(b)+"="+str(a/b))
