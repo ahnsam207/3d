e8d8783d66b13cde5df4cc4d9e030258f3105b8f
ahnsam207 <2ok25@daum.net>
2025-09-16 14:06:24 +0900

    Upload 10516정은재_0916_6.py

---
+++ upload/class5/10516정은재_0916_6.py
@@ -0,0 +1,125 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+KeyboardInterrupt
+input()
+a
+'a'
+days = input()
+5
+print(days)
+5
+def log(msg:str):
+    print(msg)
+
+
+log(asdf)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    log(asdf)
+NameError: name 'asdf' is not defined
+log("a")
+a
+days2 = str(input())
+
+d
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    d
+NameError: name 'd' is not defined. Did you mean: 'id'?
+days2()
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    days2()
+TypeError: 'str' object is not callable
+days2("a")
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    days2("a")
+TypeError: 'str' object is not callable
+asdf = char(a)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    asdf = char(a)
+NameError: name 'char' is not defined. Did you mean: 'chr'?
+asdf = chr(a)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    asdf = chr(a)
+NameError: name 'a' is not defined
+asdf = char
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    asdf = char
+NameError: name 'char' is not defined. Did you mean: 'chr'?
+asdf = chr
+chr(a)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    chr(a)
+NameError: name 'a' is not defined
+char("a")
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    char("a")
+NameError: name 'char' is not defined. Did you mean: 'chr'?
+chr("a")
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    chr("a")
+TypeError: 'str' object cannot be interpreted as an integer
+chr('d')
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    chr('d')
+TypeError: 'str' object cannot be interpreted as an integer
+asdf(1)
+'\x01'
+>>> asdf(me)
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    asdf(me)
+NameError: name 'me' is not defined
+>>> a2 = tests
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    a2 = tests
+NameError: name 'tests' is not defined
+>>> str = tests
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    str = tests
+NameError: name 'tests' is not defined
+>>> asdf = "a"
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> log("asdf")
+asdf
+>>> console.log
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    console.log
+NameError: name 'console' is not defined
+>>> class console:
+...         def logger(msg:str):
+...             print(msg)
+... 
+...             
+>>> 
+>>> console.logger("a")
+a
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> days_int = int(input('일 수를 입력하세요: '))
+일 수를 입력하세요: 5
