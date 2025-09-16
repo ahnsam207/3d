feb9560aad8cdc43fe5b322eec7390a20fc39719
ahnsam207 <2ok25@daum.net>
2025-09-16 14:05:53 +0900

    Upload 10508배재윤_0916__6.py

---
+++ upload/class5/10508배재윤_0916__6.py
@@ -0,0 +1,35 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> intput()
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    intput()
+NameError: name 'intput' is not defined. Did you mean: 'input'?
+>>> input()
+5
+'5'
+>>> days = input()
+5
+>>> days
+'5'
+>>> 5+days
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    5+days
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 5+int(days)
+10
+>>> input()
+
+''
+>>> input('나이를 입력하세요:')
+나이를 입력하세요:19
+'19'
+>>> days=input('일수를 입력하세요.')
+일수를 입력하세요.5
+>>> days
+'5'
+>>> days = int(input('일수를 입력하세요 :'))
+일수를 입력하세요 :5
+>>> days
+5
