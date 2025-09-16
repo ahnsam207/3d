520e3b686d3247678ef3855393c8ab1a448cde8d
ahnsam207 <2ok25@daum.net>
2025-09-16 16:12:41 +0900

    Upload 10417주민재_0916_7.py

---
+++ upload/class4/10417주민재_0916_7.py
@@ -0,0 +1,9 @@
+a = int(input( "첫번쩨 숫자 입력:"))
+b = int(input( "두번쩨 숫자 입력:"))
+
+
+print(str(a) + '+' + str(b) + "=" + str(a + b))
+print(str(a) + '-' + str(b) + "=" + str(a - b))
+print(str(a) + '/' + str(b) + "=" + str(a / b))
+print(str(a) + '*' + str(b) + "=" + str(a * b))
+
