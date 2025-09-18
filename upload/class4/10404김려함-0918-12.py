aef137a2e6d3f455dd21649f8721ba226f1483ec
ahnsam207 <2ok25@daum.net>
2025-09-18 10:31:26 +0900

    Upload 10404김려함-0918-12.py

---
+++ upload/class4/10404김려함-0918-12.py
@@ -0,0 +1,24 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10404김려함-0918-10.py
+투구 속도 :
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10404김려함-0918-10.py
+투구 속도 :96
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python313/10404김려함-0918-10.py", line 2, in <module>
+    print("투구의 투구 속도는" +str(round(투구속도  *1.609,1)+"입니다"))
+TypeError: unsupported operand type(s) for +: 'float' and 'str'
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10404김려함-0918-10.py
+투구 속도 :96
+투구의 투구 속도는154.5입니다
+>>> 
+============== RESTART: C:/Users/user/Desktop/10404김려함_0918_11.py ==============
+면적 입력 (제곱미터 ):85
+85 제곱미터는25.712 평입니다.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10404김려함-0918-12.py
+이름 입력 : 김려함 
+학년 입력 :3
+>>> 
