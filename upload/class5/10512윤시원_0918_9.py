30a085d33fe84c2bc7a40306352c2e00134f6a75
ahnsam207 <2ok25@daum.net>
2025-09-18 09:31:29 +0900

    Upload 10512윤시원_0918_9.py

---
+++ upload/class5/10512윤시원_0918_9.py
@@ -0,0 +1,20 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> "윤시원"
+'윤시원'
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10512_0918_8.py
+윤시원
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10512_0918_8.py ===============
+윤시원
+============================== RESTART: C:/Users/user/Desktop/10512윤시원_0918_8.py =============================
+점수입력:95
+점수는190입니다.
+>>> 
+============= RESTART: C:/Users/user/Desktop/10512윤시원_0918_9.py =============
+이름 입력:윤ㄴ시원
+당신의윤ㄴ시원입니다
+>>> 
+============= RESTART: C:/Users/user/Desktop/10512윤시원_0918_9.py =============
+이름 입력:윤시원
+당신의이름은윤시원입니다
