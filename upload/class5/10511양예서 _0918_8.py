b39d01c55b64ae7d3ed02d22a0afcf6ca34a88ea
ahnsam207 <2ok25@daum.net>
2025-09-18 09:29:49 +0900

    Upload 10511양예서 _0918_8.py

---
+++ upload/class5/10511양예서 _0918_8.py
@@ -0,0 +1,58 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10511양예서 _0918_8.py", line 1, in <module>
+    infut("이름입력")
+NameError: name 'infut' is not defined. Did you mean: 'input'?
+
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력 
+김경복
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    김경복
+NameError: name '김경복' is not defined
+
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력
+당신은 이름입력 입니다
+
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력 김경복
+당신은 이름입력 입니다
+
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10511양예서 _0918_8.py", line 2, in <module>
+    print("당신은 " + input + " 입니다")
+TypeError: can only concatenate str (not "builtin_function_or_method") to str
+>>> 
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력김경복
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10511양예서 _0918_8.py", line 2, in <module>
+    print("당신은 " + input + " 입니다")
+TypeError: can only concatenate str (not "builtin_function_or_method") to str
+>>> 
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력김경복
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10511양예서 _0918_8.py", line 2, in <module>
+    print("당신은 " + i이름 + " 입니다")
+NameError: name 'i이름' is not defined. Did you mean: '이름'?
+>>> 
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력김경복
+당신은 김경복 입니다
+>>> 
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+이름입력: 김경복
+당신은 김경복 입니다
+>>> 
+============== RESTART: C:/Users/user/Desktop/10511양예서 _0918_8.py ==============
+점수입력: 95
+점수는 190 입니다.
+>>> 
