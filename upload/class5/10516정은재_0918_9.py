136022ae47a20122bf1f46dd7800cae042cf761c
ahnsam207 <2ok25@daum.net>
2025-09-18 09:30:04 +0900

    Upload 10516정은재_0918_9.py

---
+++ upload/class5/10516정은재_0918_9.py
@@ -0,0 +1,34 @@
+class logger:
+
+    def info(msg:str):
+        print(msg)
+
+
+
+class NamePrinter:
+
+    def nameP():
+
+        name = input("이름을 입력하세요:")
+        logger.info("당신의 이름은" + name + "입니다.")
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
