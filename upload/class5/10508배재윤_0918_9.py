eee9748905dfcc0b222502a0352c673ff9e79396
ahnsam207 <2ok25@daum.net>
2025-09-18 09:31:38 +0900

    Upload 10508배재윤_0918_9.py

---
+++ upload/class5/10508배재윤_0918_9.py
@@ -0,0 +1,22 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0918_9.py ==============
+이름 입력
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0918_9.py ==============
+이름 입력:배재윤
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0918_9.py", line 2, in <module>
+    print("당신의"+ "이름은" + 이름 + "입니다")
+NameError: name '이름' is not defined
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0918_9.py ==============
+이름 입력:배재윤
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0918_9.py", line 2, in <module>
+    print("당신의"+ "이름은" + 이름입력 + "입니다")
+NameError: name '이름입력' is not defined
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10508배재윤_0918_9.py ==============
+이름 입력:배재윤
+당신의이름은배재윤입니다
