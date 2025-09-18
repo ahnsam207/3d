4ad1249fd100c9e1eb7b126bbf92692d8d08ca79
ahnsam207 <2ok25@daum.net>
2025-09-18 09:30:27 +0900

    Upload 10503김하윤_0918_9.py

---
+++ upload/class5/10503김하윤_0918_9.py
@@ -0,0 +1,6 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=============== RESTART: C:/Users/user/Desktop/10503김하윤_0918_9.py ==============
+이름입력김하윤
+당신은김하윤입니다
