782e622e04473265bfffffbfad133fc55b4f527a
ahnsam207 <2ok25@daum.net>
2025-09-18 09:31:56 +0900

    Upload 10510심대성_0918_9.py

---
+++ upload/class5/10510심대성_0918_9.py
@@ -0,0 +1,15 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10510심대성_0918_9.py
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10510심대성_0918_9.py
+>>> dkssud
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    dkssud
+NameError: name 'dkssud' is not defined
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10510심대성_0918_9.py
+이름 입력:심대성
+당신의이름은심대성입니다.
