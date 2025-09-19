67b6cb80ffe9e3aaf29425de46112ad1ffa53a3e
ahnsam207 <2ok25@daum.net>
2025-09-19 09:30:24 +0900

    Upload 10503김하윤_0919_10.py

---
+++ upload/class5/10503김하윤_0919_10.py
@@ -0,0 +1,25 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============== RESTART: C:/Users/user/Desktop/10503김하윤_0919_10.py ==============
+투구 속도 입력(마일):
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10503김하윤_0919_10.py", line 2, in <module>
+    투구속도=int(투구속도)*1.609
+ValueError: invalid literal for int() with base 10: ''
+>>> 
+============== RESTART: C:/Users/user/Desktop/10503김하윤_0919_10.py ==============
+투구 속도 입력(마일):96
+투수의 투구속도는154.464입니다.
+>>> 
+============== RESTART: C:/Users/user/Desktop/10503김하윤_0919_10.py ==============
+투구 속도 입력(마일):96
+투수의 투구속도는154km입니다.
+>>> 
+============== RESTART: C:/Users/user/Desktop/10503김하윤_0919_10.py ==============
+투구 속도 입력(마일):96
+투수의 투구속도는154.5km입니다.
+>>> 
+============== RESTART: C:/Users/user/Desktop/10503김하윤_0919_10.py ==============
+투구 속도 입력(마일):69
+투수의 투구속도는111.0km입니다.
