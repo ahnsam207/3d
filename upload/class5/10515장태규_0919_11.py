4567d71674ca1bcc13879dbc2adf1cd5a4881a6e
ahnsam207 <2ok25@daum.net>
2025-09-19 09:26:13 +0900

    Upload 10515장태규_0919_11.py

---
+++ upload/class5/10515장태규_0919_11.py
@@ -0,0 +1,14 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/user/Desktop/10515장태규_0919_10.py
+투구 속도 입력(마일) : 96
+투구의 투구 속도는97.6KM입니다.
+>>> 
+= RESTART: C:/Users/user/Desktop/10515장태규_0919_10.py
+투구 속도 입력(마일) : 96
+투구의 투구 속도는154.5KM입니다.
+>>> 
+= RESTART: C:/Users/user/Desktop/10515장태규_0919_10.py
+면적 입력 (제곱미터)85
+85제곱미터25.7평입니다. 
