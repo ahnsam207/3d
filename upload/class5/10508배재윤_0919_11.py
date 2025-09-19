6a3b9cd3ba226e52c3c812a9c42d3107fb71b744
ahnsam207 <2ok25@daum.net>
2025-09-19 09:26:14 +0900

    Upload 10508배재윤_0919_11.py

---
+++ upload/class5/10508배재윤_0919_11.py
@@ -0,0 +1,49 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+면적 입력(제곱미터) :85
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0919_11.py", line 2, in <module>
+    제곱미터=int(입력*0.3025)
+TypeError: can't multiply sequence by non-int of type 'float'
+
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+면적 입력(제곱미터) :85
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0919_11.py", line 2, in <module>
+    제곱미터=int(입력*0.3025)
+TypeError: can't multiply sequence by non-int of type 'float'
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+면적 입력(제곱미터) :85
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0919_11.py", line 2, in <module>
+    제곱미터=round(입력*0.3025,3)
+TypeError: can't multiply sequence by non-int of type 'float'
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+제곱미터:85
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0919_11.py", line 2, in <module>
+    제곱미터=round(입력*0.3025,3)
+TypeError: can't multiply sequence by non-int of type 'float'
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+제곱미터는85
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0919_11.py", line 2, in <module>
+    제곱미터=round(입력*0.3025,3)
+TypeError: can't multiply sequence by non-int of type 'float'
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+제곱미터는85
+>>> 
+>>> 
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+제곱미터는85
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_0919_11.py ==============
+제곱미터는85
+85제곱미터는25.712평입니다.
