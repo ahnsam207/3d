c99caba790dd28aa12c2cd72ed3a52d3436ecb99
ahnsam207 <2ok25@daum.net>
2025-09-22 09:29:12 +0900

    Upload 10508배재윤_0922_12py.py

---
+++ upload/class5/10508배재윤_0922_12py.py
@@ -0,0 +1,64 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0922_12py.py", line 3, in <module>
+    print="당신은"+학년입력+"학년," +이름입력+"입니다."
+NameError: name '학년입력' is not defined
+
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+김경복
+3
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0922_12py.py", line 3, in <module>
+    print="당신은"+학년입력+"학년," +이름입력+"입니다."
+NameError: name '학년입력' is not defined
+
+
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+이름입력배재윤
+학년입력3
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0922_12py.py", line 3, in <module>
+    print="당신은"+학년입력+"학년," +이름입력+"입니다."
+NameError: name '학년입력' is not defined
+
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+이름입력
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0922_12py.py", line 1, in <module>
+    이름=nput("이름입력:")
+NameError: name 'nput' is not defined. Did you mean: 'input'?
+>>> 
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0922_12py.py", line 1, in <module>
+    이름=nput("이름입력:")
+NameError: name 'nput' is not defined. Did you mean: 'input'?
+>>> 
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_0922_12py.py", line 1, in <module>
+    이름=nput("이름입력:")
+NameError: name 'nput' is not defined. Did you mean: 'input'?
+>>> 
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+이름입력:김경복
+학년입력:3
+당신은3학년,김경복입니다.
+>>> 
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+이름입력:배쟁ㄴ
+학년입력:3
+당신은3학년,배쟁ㄴ입니다.
+상품가격:10000
+할인가격은(8500.0, 1)입니다
+>>> 
+============= RESTART: C:/Users/user/Desktop/10508배재윤_0922_12py.py =============
+이름입력:배재윤
+학년입력:3
+당신은3학년,배재윤입니다.
+상품가격:100000
+할인가격은85000.0입니다
