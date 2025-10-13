55db88d706e4861b795bab90cdc31399e32e16fe
ahnsam207 <2ok25@daum.net>
2025-10-13 12:27:24 +0900

    Upload 10402김도연_1013_15.py

---
+++ upload/class4/10402김도연_1013_15.py
@@ -0,0 +1,8 @@
+name = input("input name:")
+score = int(input("input score:"))
+
+print(name+" 님의 성적은 "+str(score)+" 점입니다.")
+print(name,"님의 성적은",score,"점입니다.")
+print("{} 님의 성적은 {} 점입니다.".format(name,score))
+print(f"{name} 님의 성적은 {score} 점입니다.")
+
