c60a49eeaaa3a7d89d1ab9a2dd792274d841f5c7
ahnsam207 <2ok25@daum.net>
2025-10-13 12:27:18 +0900

    Upload 10401김단풍_1013_16.py

---
+++ upload/class4/10401김단풍_1013_16.py
@@ -0,0 +1,8 @@
+kor = int(input("kor:"))
+eng = int(input("eng:"))
+score = (kor + eng)
+total = round(score/2,1)
+
+print("당신의 평균은 " + str(total) + "입니다.")
+print("당신의 평균은",total,"입니다.")
+print(f"당신의 평균은 {total}입니다.")
