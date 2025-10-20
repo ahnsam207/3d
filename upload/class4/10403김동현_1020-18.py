427d8eb08386912c1fba9cf2dd9e2e534b31b995
ahnsam207 <2ok25@daum.net>
2025-10-20 14:07:40 +0900

    Upload 10403김동현_1020-18.py

---
+++ upload/class4/10403김동현_1020-18.py
@@ -0,0 +1,13 @@
+kor = int(input("국어:"))
+          
+eng = int(input("영어:"))
+          
+총점 = kor + eng
+          
+평균 = round(총점/2,1)
+          
+if(평균 >= 90) :
+          print("우수상")
+else:
+    print("노력상")
+
