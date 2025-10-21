3a1039a618ec9ce5c10e484d8ddbdbaad4d7b0d5
ahnsam207 <2ok25@daum.net>
2025-10-21 15:50:50 +0900

    Upload 10415이서준_1021_22.py

---
+++ upload/class4/10415이서준_1021_22.py
@@ -0,0 +1,13 @@
+국어 = int(input('국어'))
+영어 = int(input('영어'))
+평균 = round((영어 + 국어)/2,1)
+if(평균 >= 90):
+    print('A')
+elif(평균 >= 80):
+    print('B')
+elif(평균 >= 70):
+    print('C')
+elif(평균 >= 60):
+    print('D')
+else:
+    print('F')
