78c0358501ceb4618f645c60616878f51782b925
ahnsam207 <2ok25@daum.net>
2025-10-21 14:03:40 +0900

    Upload 10508배재윤_1021_22.py

---
+++ upload/class5/10508배재윤_1021_22.py
@@ -0,0 +1,14 @@
+국어=int(input("국어성적:"))
+영어=int(input("영어성적:"))
+평균=(국어+영어)/2
+if 평균>=90:
+    print("A")
+elif 평균>=80:
+    print("B")
+elif 평균>=70:
+    print("C")
+elif 평균>=60:
+    print("D")
+else:
+    print("F")
+ 
