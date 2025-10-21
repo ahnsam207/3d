1852a021b855bfc4a5260ab6867794b511344218
ahnsam207 <2ok25@daum.net>
2025-10-21 13:54:20 +0900

    Upload 10515장태규_1021_22.py

---
+++ upload/class5/10515장태규_1021_22.py
@@ -0,0 +1,15 @@
+국어 = int(input('국어 성적 입력:'))
+영어 = int(input('영어 성적 입력:'))
+총점 = 영어 + 국어
+평균 = 총점/2
+
+if 평균>=90:
+    print('A')
+elif 평균 >=80:
+     print('B')
+elif 평균 >=70:
+     print('C')
+elif 평균 >=60:
+     print('D')
+else: 
+     print('F')
