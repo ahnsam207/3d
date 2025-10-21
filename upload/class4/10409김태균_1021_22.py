0206ac5dc503db63590c01c0e1cba5d60f9da0d5
ahnsam207 <2ok25@daum.net>
2025-10-21 16:10:56 +0900

    Upload 10409김태균_1021_22.py

---
+++ upload/class4/10409김태균_1021_22.py
@@ -0,0 +1,15 @@
+국어 = int(input("국어 성적 입력:"))
+영어 = int(input("영ㅇ 성적 입력:"))
+총점 = 국어 + 영어
+평균 = round(총점/2,1)
+
+if 평균>= 90:
+    print("A")
+elif 평균>=80 :
+    print("B")
+elif 평균>=70 :
+    print("C")
+elif 평균>= 60 :
+    print("D")
+else :
+    print("F")
