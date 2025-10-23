11a532a8ef79ff4b9136e75e20aa45cfe03a1846
ahnsam207 <2ok25@daum.net>
2025-10-23 10:30:46 +0900

    Upload 10413_1023_28.py

---
+++ upload/class4/10413_1023_28.py
@@ -0,0 +1,23 @@
+import random
+
+a = int(input("홀(1),짝(2)을 선택하세요:"))
+
+com = random.randint(1,2)
+
+if (com == 1):
+    com1 = '홀'
+else :
+    com1 = '짝'
+
+if (a == 1):
+    a1 = '홀'
+else :
+    a1 = '짝'
+    
+
+if (a == com):
+    print (f"컴퓨터는 {com1} 당신은 {a1} *성공* 입니다!")
+else :
+    print (f"컴퓨터는 {com1} 당신은 {a1} *실패* 입니다!")
+    
+
