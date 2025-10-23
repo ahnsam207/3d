434cd756a11a9835c88e1d50a4969c0894104dcc
ahnsam207 <2ok25@daum.net>
2025-10-23 10:23:34 +0900

    Upload 10403김동현_1023_28.py

---
+++ upload/class4/10403김동현_1023_28.py
@@ -0,0 +1,11 @@
+import random
+
+com = random.randint(1,2)
+
+도연 = int(input("홀(1),짝(2)을 선택하세요:"))
+
+if 도연 == com:
+    print(f"컴퓨터는 {com}, 당신은 {도연} *성공*입니다.")
+
+else:
+    print(f"컴퓨터는 {com}, 당신은 {도연} *실패*입니다.")
