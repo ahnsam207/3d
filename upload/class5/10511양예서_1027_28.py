f1f23f6a41d2bd857caebffa683c2cdf0faee210
ahnsam207 <2ok25@daum.net>
2025-10-27 09:30:38 +0900

    Upload 10511양예서_1027_28.py

---
+++ upload/class5/10511양예서_1027_28.py
@@ -0,0 +1,16 @@
+import random
+com = random.randint(1,2)
+user = input("홀(1), 짝(2)을 선택하세요:")
+if com == user:
+    if user == 1:
+        print("컴퓨터는 홀, 당신은 홀*성공*입니다.")
+    else:
+        print("컴퓨터는 짝, 당신은 짝*성공*입니다.")
+else:
+    if user == 1:
+        print("컴퓨터는 짝, 당신은 홀 *실패*입니다.")
+    else :
+        print("컴퓨터는 홀, 당신은 짝 *실패*입니다.")
+
+           
+        
