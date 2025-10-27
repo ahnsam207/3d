ed485d44fb6c0adac5832d9e4273670409ae712a
ahnsam207 <2ok25@daum.net>
2025-10-27 09:29:12 +0900

    Upload 10512윤시원_1027_28.py

---
+++ upload/class5/10512윤시원_1027_28.py
@@ -0,0 +1,16 @@
+import random
+com = random.randint(1,2)
+
+선택 = int(input('홀(1) 짝(2)을 선택하세요.: '))
+
+if com == 선택:
+    if 선택 == 1:
+        print(f'컴퓨터는 홀, 당신은 홀 *성공*입니다.')
+    
+    else:
+        print(f'컴퓨터는 짝, 당신은 짝 *성공*입니다.')
+else:
+    if 선택 == 1:
+        print(f'컴퓨터는 짝, 당신은 홀 *실패*입니다.')
+    else:
+        print(f'컴퓨터는 홀, 당신은 짝 *실패*입니다.')
