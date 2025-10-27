8be8751ae1d2a3d2066a5e5f37ee72d0b017fade
ahnsam207 <2ok25@daum.net>
2025-10-27 09:31:18 +0900

    Upload 10503김하윤_1027_28.py

---
+++ upload/class5/10503김하윤_1027_28.py
@@ -0,0 +1,11 @@
+import random
+
+com = random.randint(1,2)
+
+숫자=int(input("홀(1),짝(2)을 선택하세요 :"))
+if 숫자==com:
+    print(f"컴퓨터는{com}, 당신은{숫자} *성공*입니다.")
+else:
+    print(f"컴퓨터는 {com}, 당신은 {숫자} *실패*입니다.")
+          
+    
