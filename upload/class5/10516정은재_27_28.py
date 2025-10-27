f4406b3e6578561e7cf1eb18295fc83b9903ad86
ahnsam207 <2ok25@daum.net>
2025-10-27 09:30:11 +0900

    Upload 10516정은재_27_28.py

---
+++ upload/class5/10516정은재_27_28.py
@@ -0,0 +1,19 @@
+import random
+
+
+com = random.randint(1,2)
+
+prompt = int(input("홀(1), 짝(2)을 선택하세요\n"))
+
+if com == prompt:
+    if prompt == 1:
+        print("컴퓨터는 홀, 당신은 홀 *성공* 입니다.")
+    else:
+        print("컴퓨터는 홀, 당신은 짝 *실패* 입니다.")
+else:
+    if prompt == 1:
+        print("컴퓨터는 짝, 당신은 홀 *실패* 입니다.")
+    else:
+        print("컴퓨터는 짝, 당신은 짝 *성공* 입니다.")
+
+    
