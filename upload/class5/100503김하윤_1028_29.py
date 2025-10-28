fbba40f5892eaf2a4c1ab7b856cb45393c4f2edb
ahnsam207 <2ok25@daum.net>
2025-10-28 14:01:41 +0900

    Upload 100503김하윤_1028_29.py

---
+++ upload/class5/100503김하윤_1028_29.py
@@ -0,0 +1,26 @@
+import random
+com = random.randint(1,3)
+user=int(input("가위(1),바위(2),보(3)를 선택하세요:"))
+if user==1:
+    if com==1:
+        print("컴퓨터는 가위, 당신은 가위 비겼습니다")
+    if com==2:
+        print("컴퓨터는 바위, 당신은 가위 졌습니다")
+    if com==3:
+        print("컴퓨터는 보, 당신은 가위 이겼습니다")
+elif user==2:
+    if com==1:
+        print("컴퓨터는 가위, 당신은 바위 이겼습니다")
+    if com==2:
+        print("컴퓨터는 바위, 당신은 바위 비겼습니다")
+    if com==3:
+        print("컴퓨터는 보, 당신은 바위 졌습니다")
+elif user==3:
+    if com==1:
+        print("컴퓨터는 가위, 당신은 보 졌습니다")
+    if com==2:
+        print("컴퓨터는 바위, 당신은 보 이겼습니다")
+    if com==3:
+        print("컴퓨터는 보, 당신은 보 비겼습니다")
+else:
+    print("다시 입력하세요.")
