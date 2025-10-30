e7665bae4f6e2d50ef09813a222efa3de85fe989
ahnsam207 <2ok25@daum.net>
2025-10-30 10:31:26 +0900

    Upload 10411박서연_1030_33.py

---
+++ upload/class4/10411박서연_1030_33.py
@@ -0,0 +1,7 @@
+import random
+print(f"{random.randint(1,45)} / {random.randint(1,45)} / {random.randint(1,45)} / {random.randint(1,45)} / {random.randint(1,45)} / {random.randint(1,45)}")
+
+lst = list(random.randint(1,45))
+
+if random.randint(1,45)==random.randint(1,45):
+    list.remove()
