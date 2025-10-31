85a77265ad42a0b01b8769cc13cf832ced0aa489
ahnsam207 <2ok25@daum.net>
2025-10-31 09:29:55 +0900

    Upload 10516정은재_1031_31.py

---
+++ upload/class5/10516정은재_1031_31.py
@@ -0,0 +1,27 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+lst = []
+lst
+[]
+>>> lst1 = list()
+>>> lst.append(90)
+>>> lst.append(85)
+>>> lst.append(97)
+>>> lst.sort()
+>>> lst.reverse()
+>>> lst
+[97, 90, 85]
+>>> lst.index(90)
+1
+>>> lst.count(90)
+1
+>>> lst.append(90)
+>>> lst.count(90)
+2
+>>> lst1.append("유재석")
+>>> lst1.append("박명수")
+>>> lst1.append("하하")
+>>> lst
+[97, 90, 85, 90]
+>>> lst1.sort()
+>>> lst1.reverse()
