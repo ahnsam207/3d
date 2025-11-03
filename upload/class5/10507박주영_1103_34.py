17e8c85708c371f2aac34b20b138989f3660976a
ahnsam207 <2ok25@daum.net>
2025-11-03 09:31:17 +0900

    Upload 10507박주영_1103_34.py

---
+++ upload/class5/10507박주영_1103_34.py
@@ -0,0 +1,45 @@
+import random
+#lotto = [1,2,3,4,5,6,7,8,9,10,11,...]
+lotto=[i for i in range(1,46)]
+temp = ""
+result=""
+select = []
+
+random.shuffle(lotto)
+temp = lotto.pop()
+select.append(temp)
+result = result + str(temp)
+
+random.shuffle(lotto)
+temp = lotto.pop()
+select.append(temp)
+result = result + " / " + str(temp)
+
+random.shuffle(lotto)
+temp = lotto.pop()
+select.append(temp)
+result = result + " / " + str(temp)
+
+random.shuffle(lotto)
+temp = lotto.pop()
+select.append(temp)
+result = result + " / " + str(temp)
+
+random.shuffle(lotto)
+temp = lotto.pop()
+select.append(temp)
+result = result + " / " + str(temp)
+
+random.shuffle(lotto)
+temp = lotto.pop()
+select.append(temp)
+result = result + " / " + str(temp)
+
+random.shuffle(lotto)
+temp = lotto.pop()
+select.append(temp)
+result = result + " / " + str(temp)
+
+print("LOTTO 번호 추출")
+print(f"리스트 : {select[0]} / {select[1]} / {select[2]} / {select[3]} / {select[4]} / {select[5]}")
+print(f"문자열 : {result}")
