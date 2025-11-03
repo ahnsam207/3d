ec759525d54cf53b6255517b1a026605452525df
ahnsam207 <2ok25@daum.net>
2025-11-03 09:29:23 +0900

    Upload 10509신정익_1103_34.py

---
+++ upload/class5/10509신정익_1103_34.py
@@ -0,0 +1,22 @@
+import random
+
+#lotto = [1,2,3,4,5,6,7,8,9,10,11,...]
+lpttp = [i for i in range(1,46)]
+
+print(lotto)
+
+random.shuffle(lotto)
+select.append(lotto.pop())
+
+random.shuffle(lotto)
+select.append(lotto.pop())
+
+random.shuffle(lotto)
+select.append(lotto.pop())
+
+random.shuffle(lotto)
+select.append(lotto.pop())
+
+random.shuffle(lotto)
+select.append(lotto.pop())
+ 
