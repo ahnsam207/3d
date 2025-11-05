3fe859b53ca113faf9a8b3a5dbc01b5f22a4683d
ahnsam207 <2ok25@daum.net>
2025-11-05 10:31:07 +0900

    Upload 10401김단풍_1105_34.py.py

---
+++ upload/class4/10401김단풍_1105_34.py.py
@@ -0,0 +1,24 @@
+import random
+
+lotto = list(range(1, 46))
+
+result = ""
+random.shuffle(lotto)
+result = result + str(lotto.pop())
+
+random.shuffle(lotto)
+result = result + " / " + str(lotto.pop())
+
+random.shuffle(lotto)
+result = result + " / " + str(lotto.pop())
+
+random.shuffle(lotto)
+result = result + " / " + str(lotto.pop())
+
+random.shuffle(lotto)
+result = result + " / " + str(lotto.pop())
+
+random.shuffle(lotto)
+result = result + " / " + str(lotto.pop())
+
+print(result)
