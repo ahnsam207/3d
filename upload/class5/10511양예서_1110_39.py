2066740a7d5451fb0565abe1a00a396fadfae99f
ahnsam207 <2ok25@daum.net>
2025-11-10 09:28:13 +0900

    Upload 10511양예서_1110_39.py

---
+++ upload/class5/10511양예서_1110_39.py
@@ -0,0 +1,24 @@
+i = 0
+while 0 <= 4:
+    print('경복')
+    i = 0 + 1
+
+while 1 <= 4:
+    print('경복')
+    i = 1 + 1
+
+while 2 <= 4:
+    print('경복')
+    i = 2 + 1
+
+while 3 <= 4:
+    print('경복')
+    i = 3 + 1
+
+while 4 <= 4:
+    print('경복')
+    i = 4 + 1
+
+while 5 <= 4:
+    print('경복')
+    i = 5 + 1
