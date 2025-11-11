7a9ed8cb72714bf7fe6951f6507de8ed3b5ca1e7
ahnsam207 <2ok25@daum.net>
2025-11-11 14:04:40 +0900

    Upload 10513이기범_1111_43.py

---
+++ upload/class5/10513이기범_1111_43.py
@@ -0,0 +1,15 @@
+이름=[]
+while True:
+     temp=input("이름 입력(종료 q 입력):")
+     if temp == 'q':
+         break
+     else:
+         이름.append(temp)
+name=input("찾을 학생 이름 입력:")
+for i,name in enumerate(이름):
+    if l_name == name:
+        print(f"학생은"+str(i+1)+"번입니다.")
+        break
+    else:
+        if i==ien(이름)-1:
+            print("학생이 없습니다.")
