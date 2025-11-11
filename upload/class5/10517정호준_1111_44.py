f90243989e5ac974c8e135afb068801bcdca9bc5
ahnsam207 <2ok25@daum.net>
2025-11-11 14:09:42 +0900

    Upload 10517정호준_1111_44.py

---
+++ upload/class5/10517정호준_1111_44.py
@@ -0,0 +1,53 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+... math_list = [5,6,7,8]
+... print(abs(-3))
+... print(abs(3))
+... print(pow(2,3))
+... print(max(2,3,4,5))
+... print(max(math_list))
+... print(min(2,3,4,5))
+... print(min(2,3,4,5))
+... print(sum(math_list))
+SyntaxError: multiple statements found while compiling a single statement
+>>> math_list = [5,6,7,8]
+>>> print(abs(-3))
+... print(abs(3))
+... print(pow(2,3))
+... print(max(2,3,4,5))
+... print(max(math_list))
+... print(min(2,3,4,5))
+... print(min(2,3,4,5))
+... print(sum(math_list))
+SyntaxError: multiple statements found while compiling a single statement
+>>> print(sum(math_list))
+26
+>>> 
+>>> print(abs(-3))
+... print(abs(3))
+... print(pow(2,3))
+... print(max(2,3,4,5))
+... print(max(math_list))
+... print(min(2,3,4,5))
+... print(min(2,3,4,5))
+... print(sum(math_list))
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> 
+>>> 
+>>> 
+>>> print(abs(-3))
+... print(abs(3))
+... print(pow(2,3))
+... print(max(2,3,4,5))
+... print(max(math_list))
+... 
+... 
+... print(min(2,3,4,5))
+... # error print(sum(2,3))
+... 
+... print(min(2,3,4,5))
+... print(sum(math_list))
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
