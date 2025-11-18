428194fdc3670f697052394dc02a40e6df01f4c3
ahnsam207 <2ok25@daum.net>
2025-11-18 14:07:45 +0900

    Upload 10508배재윤_1118_47.py

---
+++ upload/class5/10508배재윤_1118_47.py
@@ -0,0 +1,100 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============== RESTART: C:/Users/user/Desktop/10508배재윤_1118_45.py ==============
+반복 횟수:3
+값 입력:3
+v:3,c:1,s:3
+값 입력:
+============== RESTART: C:/Users/user/Desktop/10508배재윤_1118_45.py ==============
+반복 횟수:a
+반복 횟수:a
+Traceback (most recent call last):
+  File "C:/Users/user/Desktop/10508배재윤_1118_45.py", line 6, in <module>
+    a = int(input("반복 횟수:"))
+ValueError: invalid literal for int() with base 10: 'a'
+a
+'a'
+a
+
+a
+
+a
+a
+
+a
+'a'
+
+a
+
+a
+
+a
+'a'a
+a
+'a'
+
+a
+a
+'a'
+
+
+a
+a
+
+a
+
+a
+a
+
+a
+'a'
+a
+
+============== RESTART: C:/Users/user/Desktop/10508배재윤_1118_45.py ==============
+반복 횟수:3
+값 입력:3
+v:3,c:1,s:3
+값 입력:
+============== RESTART: C:/Users/user/Desktop/10508배재윤_1118_45.py ==============
+반복 횟수:
+    3
+반복 횟수:반복 횟수:
+============== RESTART: C:/Users/user/Desktop/10508배재윤_1118_45.py ==============
+반복 횟수:3
+값 입력:3
+v:3,c:1,s:3
+값 입력:3
+v:3,c:2,s:6
+값 입력:5
+v:5,c:3,s:11
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_1118_45.py ==============
+반복 횟수:2
+값 입력:4
+v:4,c:1,s:4
+값 입력:6
+v:6,c:2,s:10
+>>> 
+============== RESTART: C:/Users/user/Desktop/10508배재윤_1118_46.py ==============
+수1 입력:3
+수2 입력:3
+3~3:짝수합-0, 홀수합~0, 짝수-0개, 홀수6개
+짝수:[]
+홀수:[3]
+>>> student = {'학번':2025, '소속':'ai', '이름':'김경복'}
+>>> student ['이름']
+'김경복'
+>>> student
+{'학번': 2025, '소속': 'ai', '이름': '김경복'}
+>>> student['학년'] =1
+>>> student
+{'학번': 2025, '소속': 'ai', '이름': '김경복', '학년': 1}
+>>> student['학년'] = 2
+>>> student
+{'학번': 2025, '소속': 'ai', '이름': '김경복', '학년': 2}
+>>> del student['소속']
+>>> student
+{'학번': 2025, '이름': '김경복', '학년': 2}
+>>> f"내 이름은 {student['이름']}, 학년은 {student['학년']}학년 입니다."
+'내 이름은 김경복, 학년은 2학년 입니다.'
