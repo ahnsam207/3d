a31705a5c1e5fc9ac7ee52542ac99a6edc232d7d
ahnsam207 <2ok25@daum.net>
2025-11-18 14:08:42 +0900

    Upload 10504 남은서_1118_45.py

---
+++ upload/class5/10504 남은서_1118_45.py
@@ -0,0 +1,13 @@
+c = 0
+s = 0
+
+a = input("반복 횟수: ")
+while not a.isdigit():
+    a = input("반복 횟수: ")
+a = int(a)
+for i in range(a):
+    v = int(input("값 입력: "))
+    c = c+1
+    s = s+v
+    print(f"v:{v},c:{c},s:{s}")
+    
