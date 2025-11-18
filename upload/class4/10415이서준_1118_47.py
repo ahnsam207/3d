89f68dcf5740c85ba60773007589f98797a539c0
ahnsam207 <2ok25@daum.net>
2025-11-18 16:13:02 +0900

    Upload 10415이서준_1118_47.py

---
+++ upload/class4/10415이서준_1118_47.py
@@ -0,0 +1,101 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py ==============
+반복 횟수:5
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py", line 5, in <module>
+    while not a.isidit():
+AttributeError: 'str' object has no attribute 'isidit'. Did you mean: 'isdigit'?
+
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py ==============
+반복 횟수:5
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py", line 5, in <module>
+    while not a.isigit():
+AttributeError: 'str' object has no attribute 'isigit'. Did you mean: 'isdigit'?
+
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py ==============
+반복 횟수:5
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py", line 5, in <module>
+    while not a.isdgit():
+AttributeError: 'str' object has no attribute 'isdgit'. Did you mean: 'isdigit'?
+
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py ==============
+반복 횟수:5
+값 입력:1
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py", line 11, in <module>
+    s = S + v
+NameError: name 'S' is not defined. Did you mean: 's'?
+
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py ==============
+반복 횟수:5
+값 입력:d
+Traceback (most recent call last):
+  File "C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py", line 9, in <module>
+    v = int(input("값 입력:"))
+ValueError: invalid literal for int() with base 10: 'd'
+>>> 
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py ==============
+반복 횟수:
+... 
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_45.py ==============
+반복 횟수:9
+값 입력:3
+v : 3, c : 1, s : 3
+값 입력:
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_46.py ==============
+수1 입력:1
+수2 입력:1
+1~1:짝수합-0, 홀수합-1, 짝수-0개, 홀수-1개
+짝수:[]
+홀수:[1]
+>>> 
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_46.py ==============
+수1 입력:3
+수2 입력:4
+3~4:짝수합-4, 홀수합-3, 짝수-1개, 홀수-1개
+짝수:[4]
+홀수:[3]
+>>> 
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_46.py ==============
+수1 입력:99
+수2 입력:18
+99~18:짝수합-0, 홀수합-0, 짝수-0개, 홀수-0개
+짝수:[]
+홀수:[]
+>>> 
+=============== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python313/10415이서준_1118_46.py ==============
+수1 입력:1
+수2 입력:79
+1~79:짝수합-1560, 홀수합-1600, 짝수-39개, 홀수-40개
+짝수:[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78]
+홀수:[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79]
+student = {"학번":2025, "소속":"AI", "이름":"김경복"}
+student["이름"]
+'김경복'
+student
+{'학번': 2025, '소속': 'AI', '이름': '김경복'}
+이서준 = {10415"이서"준}
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+이서준 = {준 "서" 이 1041"5"}
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+학번
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    학번
+NameError: name '학번' is not defined
+'학번'
+'학번'
+이서준 = {준}
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    이서준 = {준}
+NameError: name '준' is not defined
+이서준 = {"성":"이", "이름":"서준", "학번":"10415", "학과":"Ai융합콘텐츠과", "학년":1, "반":4, "번호":15}
+이서준
+{'성': '이', '이름': '서준', '학번': '10415', '학과': 'Ai융합콘텐츠과', '학년': 1, '반': 4, '번호': 15}
+이서준["학과"]
+'Ai융합콘텐츠과'
