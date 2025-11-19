8c619fcc45a5ef302105e3446e65f4183e23480d
ahnsam207 <2ok25@daum.net>
2025-11-19 09:21:20 +0900

    Upload 10407김재현_1119_48.py

---
+++ upload/class4/10407김재현_1119_48.py
@@ -0,0 +1,16 @@
+color = {} # 빈 사진 생성
+color['빨강'] = 'Red'
+color['파랑'] = 'Blue'
+color['노랑'] = 'Yellow'
+color['보라'] = 'violet'
+color['주황'] = 'Orange'
+color['초록'] = 'Green'
+color['남색'] = 'Indigo'
+color['분홍'] = 'pink'
+color['횐색'] = 'White'
+color['검정'] = 'Black'
+# 사전 출력
+for i in color :
+    print(f"{i}는 {color[i]}입니다.")
+    
+        
