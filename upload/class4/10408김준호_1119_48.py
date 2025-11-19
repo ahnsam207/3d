00f674bcb76ffda738dd37c537f45e6fe2a70fc2
ahnsam207 <2ok25@daum.net>
2025-11-19 09:22:27 +0900

    Upload 10408김준호_1119_48.py

---
+++ upload/class4/10408김준호_1119_48.py
@@ -0,0 +1,14 @@
+c = {} # 빈 사전 생성
+c['빨강'] = 'Red'
+c['파랑'] = 'Blue'
+c['노랑'] = 'Yellow'
+c['보라'] = 'Violet'
+c['주황'] = 'Orange'
+c['초록'] = 'Green'
+c['남색'] = 'Indigo'
+c['분홍'] = 'Pink'
+c['흰색'] = 'White'
+c['검정'] = 'Black'
+# 사전 출력
+for i in c :
+    print(f"{i}는 {c[i]} 입니다.")
