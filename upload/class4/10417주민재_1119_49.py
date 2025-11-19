6b3fde0eeff510db57b77dbdbeadbd6f6d1d6b83
ahnsam207 <2ok25@daum.net>
2025-11-19 09:21:54 +0900

    Upload 10417주민재_1119_49.py

---
+++ upload/class4/10417주민재_1119_49.py
@@ -0,0 +1,23 @@
+# 빈 사전 생성
+color = {}
+
+color['빨강'] =  'Red';                color['파랑'] = 'Blue'
+color['노랑'] = 'yellow' ;          color['노랑'] = 'yellow'
+color['주황'] = 'Orange';          color['초록'] = 'Green'
+color['남색'] = 'Indigo';          color['분홍'] = 'Pink'
+color['흰색'] = 'White';             color['검정'] = 'Black'
+
+
+# 좋아하는 색 찾기
+print("좋아하는 색을 입력하세요. 종료하려면 '끝'을 입력하세요.")
+
+while True :
+    favorite = input('당신이 좋아하는 색은?:')
+    if  favorite == '끝' :
+        print("종료합니다.")
+        break
+    elif favorite in color :
+        print(f'{favorite} - -> {color[favorite]}')
+    else :
+        print('그런 색이 없습니다.')
+        print('=========================')
