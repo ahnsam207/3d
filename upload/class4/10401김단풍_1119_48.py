fd421e927047d6d86b1977f83efcdc5eeda36628
ahnsam207 <2ok25@daum.net>
2025-11-19 09:22:14 +0900

    Upload 10401김단풍_1119_48.py

---
+++ upload/class4/10401김단풍_1119_48.py
@@ -0,0 +1,16 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> color = {}
+>>> color['빨강'] = 'Red'
+>>> color['파랑'] = 'Blue'
+>>> color['노랑'] = 'Yellow'
+>>> color['보라'] = 'Violent'
+>>> color['주황'] = 'Orange'
+>>> color['초록'] = 'Green'
+>>> color['남색'] =
+SyntaxError: invalid syntax
+>>> color['남색'] = 'Indigo'
+>>> color['분홍'] = 'Pink'
+>>> color['흰색'] = 'White'
+>>> color['검정'] = 'Black'
+>>> for i in color:
