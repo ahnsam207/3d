5f7300bece9d3071a4c0c26996c92097de01ead8
ahnsam207 <2ok25@daum.net>
2025-11-19 09:19:07 +0900

    Upload 10402김도연_1118_50.py

---
+++ upload/class4/10402김도연_1118_50.py
@@ -0,0 +1,15 @@
+import random as r
+
+sr = ['가위','바위','보']
+rule = {0:1,1:2,2:0}
+
+com = r.randrange(3)
+user = int(input("가위(0),바위(1),보(2) 중 선택:"))
+print(f"user = {sr[user]},com = {sr[com]}")
+
+if(user == com):
+    print('draw')
+elif rule[com] == user:
+    print('You Win!!')
+else:
+    print('Computer Win')
