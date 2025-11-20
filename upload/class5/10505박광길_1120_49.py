c1e4dfb36b988c72af72264c673b19796240fbbc
ahnsam207 <2ok25@daum.net>
2025-11-20 09:29:24 +0900

    Upload 10505박광길_1120_49.py

---
+++ upload/class5/10505박광길_1120_49.py
@@ -0,0 +1,7 @@
+color = {}
+color['빨강'] = 'Red'    color['파랑'] = 'Blue'
+color['노랑'] = 'yellow' color['보라'] = 'violet'
+color['주황'] = 'Orange' color['초록'] = 'Green'
+color['남색'] = 'Indigo' color['분홍'] = 'pink'
+color['흰색'] = 'white'  color['검정'] = 'Black'
+ 
