406dacdf36f8c1d8e3a0eb8d87e4afa9acbacce6
ahnsam207 <2ok25@daum.net>
2025-11-20 09:29:16 +0900

    Upload 10518 최영은_1120_48.py

---
+++ upload/class5/10518 최영은_1120_48.py
@@ -0,0 +1,15 @@
+color = {}
+color['빨강'] = 'Red'
+color['파랑'] = 'Blue'
+color['노랑'] = 'Yellow'
+color['보라'] = 'Violet'
+color['주황'] = 'Orange'
+color['초록'] = 'Green'
+color['남색'] = 'Indigo'
+color['분홍'] = 'Pink'
+color['회색'] = 'white'
+color['검정'] = 'Black'
+
+print(color)
+for i in color:
+    print('%s는 %s 입니다. ' %(i, color[i]))
