0c980bff4bbdb4064747fb9f70dad230c974f36d
ahnsam207 <2ok25@daum.net>
2025-11-20 09:28:45 +0900

    Upload 10509신정익_1120_48.py

---
+++ upload/class5/10509신정익_1120_48.py
@@ -0,0 +1,15 @@
+
+color = {}
+color['빨강'] = 'red'
+color['파랑'] = 'blue'
+color['노랑'] = 'yellow'
+color['보라'] = 'violet'
+color['주황'] = 'orange'
+color['초록'] = 'green'
+color['남색'] = 'indigo'
+color['분홍'] = 'pink'
+color['흰색'] = 'white'
+color['검정'] = 'black'
+print(color)
+for i in color :
+    print('%s는 %s입니다.'%(i, color[i]))
