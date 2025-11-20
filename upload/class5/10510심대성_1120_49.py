e06a904cb992d263c76145b34f8215a6dd124891
ahnsam207 <2ok25@daum.net>
2025-11-20 09:25:38 +0900

    Upload 10510심대성_1120_49.py

---
+++ upload/class5/10510심대성_1120_49.py
@@ -0,0 +1,21 @@
+color = {}
+
+color['빨강'] = 'red';     color['파랑'] = 'blue'
+color['노랑'] = 'yellow';  color['보라'] = 'violet'
+color['주황'] = 'orenge';  color['초록'] = 'green'
+color['남색'] = 'indigo';  color['분홍'] = 'pink'
+color['흰색'] = 'white';   color['검정'] = 'black'
+
+print('좋아하는 색을 입력하세요. 종료하면  "끝"을 입력하세요.')
+
+while True :
+    favorite == input('당신이 좋아하는 색은 ? :')
+    if favorite == "끝" :
+        print('종료합니다.')
+        break
+    elif favorite in color :
+        print(f'{favorite} --> {color[favorite]}')
+    else :
+        print('그런 색이 없습니다.')
+    print("="*30)
+        
