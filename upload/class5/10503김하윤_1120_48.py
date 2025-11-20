15aa1d0d6c7e1753829075ab109590fddb936be2
ahnsam207 <2ok25@daum.net>
2025-11-20 09:27:12 +0900

    Upload 10503김하윤_1120_48.py

---
+++ upload/class5/10503김하윤_1120_48.py
@@ -0,0 +1,15 @@
+color= {}
+color['빨강']='RED'
+color['파랑']='BLUE'
+color['노랑']='YELLOW'
+color['보라']='VIOLET'
+color['주황']='ORANGE'
+color['초록']='GREEN'
+color['남색']='INDIGO'
+color['분홍']='PINK'
+color['흰색']='WHITE'
+color['검정']='BLACK'
+
+for i in color:
+    print('%s는 %s입니다.' %(i, color[i]))
+
