2435de02553de23a67eeae5d3f7f3536bfd9071f
ahnsam207 <2ok25@daum.net>
2025-11-20 09:25:16 +0900

    Upload 10512윤시원_1120_48.py

---
+++ upload/class5/10512윤시원_1120_48.py
@@ -0,0 +1,14 @@
+color['빨강'] = 'Red'
+color['파랑'] = 'Blue'
+color['노랑'] = 'Yellow'
+color['보라'] = 'Violet'
+color['주황'] = 'Orange'
+color['초록'] = 'Green'
+color['남색'] = 'Indigo'
+color['분홍'] = 'Pink'
+color['흰색'] = 'White'
+color['검정'] = 'black'
+for i in color :
+    print('%s는 %s 입니다.' %(i,color[i]))
+    
+
