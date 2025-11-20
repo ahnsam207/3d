9544ca3158cb76cf92f138ebfd1034fa5e91b457
ahnsam207 <2ok25@daum.net>
2025-11-20 09:25:42 +0900

    Upload 10515장태규_1120_49.py

---
+++ upload/class5/10515장태규_1120_49.py
@@ -0,0 +1,24 @@
+color = {} # 빈 사전 생성
+color['빨강'] = 'Red'
+color['파랑'] = 'Blue'
+color['노랑'] = 'yellow'
+color['보라'] = 'violet'
+color['주황'] = 'Orange'
+color['초록'] = 'Green'
+color['남색'] = 'Indigo'
+color['분홍'] = 'Pink'
+color['흰색'] = 'White'
+color['검정'] = 'Black'
+
+print('좋아하는 색을 입력하세요. 종료하려면 "끝"을 입력하세요.')
+
+while True :
+    favorite = input('당신이 좋아하는 색은 ? : ')
+    if favorite == "끝" :
+       print('종료합니다.')
+       break
+    elif favorite in color :
+        print(f'{favorite}--> {color[favorite]}')
+    else :
+        print('그런 색이 없습니다.')
+    print('==========================================')
