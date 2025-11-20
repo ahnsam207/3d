f58e0b27c056225b09bc8b0e94f703312acbc3d1
ahnsam207 <2ok25@daum.net>
2025-11-20 09:29:27 +0900

    Upload 10505박광길_1120_50.py

---
+++ upload/class5/10505박광길_1120_50.py
@@ -0,0 +1,14 @@
+import random as r
+sr = ['가위', '바위', '보']
+rule = {0:1, 1:2, 2:0 }
+
+com = r.randeange(3)
+user = int(input("가위(0), 바위(1), 보(2) 중 선택:"))
+print(f'user = {sr[user]}, com = {sr[com]}')
+
+if user == com :
+    print('비겼습니다')
+elif rule[com] == user :
+    print('당신이 이겼습니다')
+else :
+    print('컴퓨터가 이겼습니다')
