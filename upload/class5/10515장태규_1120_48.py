b8df2c62bf27b17054622f2b9c602d7b6b6ca1d9
ahnsam207 <2ok25@daum.net>
2025-11-20 09:25:39 +0900

    Upload 10515장태규_1120_48.py

---
+++ upload/class5/10515장태규_1120_48.py
@@ -0,0 +1,14 @@
+color = {} # 빈 사전 생성
+color['빨강'] = 'Red'
+color['파랑'] = 'Blue'
+color['노랑'] = 'yellow'
+color['보라'] = 'violet'
+color['주황'] = 'Orange'
+color['초록'] = 'Green'
+color['남색'] = 'Indigo'
+color['분홍'] = 'Pink'
+color['흰색'] = 'White'
+color['검정'] = 'Black'
+# 사전 출력
+for i in color :
+    print('%s는 %s입니다.' %(i, color[i]))
