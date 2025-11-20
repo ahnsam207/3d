45b8fb7a22f986e72c31f1bbdcc142a38d5cc840
ahnsam207 <2ok25@daum.net>
2025-11-20 09:28:47 +0900

    Upload 0504남은서_1120_48.py

---
+++ upload/class5/0504남은서_1120_48.py
@@ -0,0 +1,14 @@
+color = {}
+color['빨강'] = 'red'
+color['파랑'] = 'blue'
+color['노랑'] = 'yellow'
+color['보라'] = 'violrt'
+color['주황'] = 'orange'
+color['초록'] = 'green'
+color['보라'] = 'violrt'
+color['남색'] = 'indigo'
+color['분홍'] = 'pink'
+color['흰색'] = 'white'
+color['검정'] = 'black'
+for i in color :
+    print('%s는 %s입니다.' %(i, color[i]))
