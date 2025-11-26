10979c4ebfe4d7a933fccfcbcde746be125eb9c9
ahnsam207 <2ok25@daum.net>
2025-11-26 10:10:48 +0900

    Upload 10407김재현_1126_58.py

---
+++ upload/class4/10407김재현_1126_58.py
@@ -0,0 +1,10 @@
+def sum2m(m) :
+    total = 0       # total 초기화
+    for k in range(1, n+1, 1) :
+        total = total + k
+    return total
+a = input('음이 아는 n값을 입력하세요 : ')
+while not a.isdigit() :
+    a = input('음이 아닌 n값을 입력하세요: ')
+a = int(a)
+print(f'1부터 {a}까지의 합은 {sum2n(a)}입니다. ')
