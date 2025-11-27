95ee919d58609c9774699e55cf399a77a811544d
ahnsam207 <2ok25@daum.net>
2025-11-27 09:11:35 +0900

    Upload 10516정은재_1127_51.py

---
+++ upload/class5/10516정은재_1127_51.py
@@ -0,0 +1,15 @@
+
+
+async def main():
+        print("안녕하세요")
+        print("경복여고입니다")
+
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+    asyncio.run(main())
+    asyncio.run(main())
+    asyncio.run(main())
+    
