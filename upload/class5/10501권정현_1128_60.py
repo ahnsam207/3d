f39df0bed9fc767e3620987dc3ccf8bf00cf2a13
ahnsam207 <2ok25@daum.net>
2025-11-28 09:26:17 +0900

    Upload 10501권정현_1128_60.py

---
+++ upload/class5/10501권정현_1128_60.py
@@ -0,0 +1,7 @@
+file_data = open("data.txt", "w",encoding = "utf-8")
+
+data_list = ["10101,강하나,75,80,90", "10102,김이번,75,85,80","10103,나삼번,90,85,95"]
+
+file_data.writelines(data_list)
+
+file_data.close()
